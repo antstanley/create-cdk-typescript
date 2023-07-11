ed738b1b714305f8eb0fc0aa1746a9528257894a
Ant Stanley <antstanley@gmail.com>
2023-07-11 17:07:21 +0200

    chore: add prettier and eslint pre-commit hooks

---
+++ .eslintrc.cjs
@@ -0,0 +1,17 @@
+module.exports = {
+  "env": {
+    "es2021": true,
+    "node": true
+  },
+  "parserOptions": {
+    "project": "tsconfig.json",
+    "ecmaVersion": "latest",
+    "sourceType": "module"
+  },
+  "extends": [
+    "standard-with-typescript",
+    "prettier"
+  ],
+  "rules": {},
+  "ignorePatterns": ".eslintrc.cjs"
+}
---
+++ index.ts
@@ -0,0 +1,20 @@
+console.log('test')
+
+function test(): void {
+  const message = " this is a test"
+  console.log(message)
+  const complexObject = {
+    prop: 'some detail',
+    obj: {
+      				prop: {
+        obj: {
+          	val: "asdas"
+        }
+      }
+    }
+  }
+
+  console.log(complexObject)
+}
+
+test()
