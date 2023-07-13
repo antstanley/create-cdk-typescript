b8f6034ad069ace687945fca47cbab0fd8d2fbe8
Ant Stanley <antstanley@gmail.com>
2023-07-13 08:34:02 +0200

    fix: use template literal for help text

--- index.ts
+++ index.ts
@@ -1,11 +1,7 @@
 import process from 'node:process';
-import { readFileSync } from 'node:fs'
-import { fileURLToPath } from 'node:url'
-import { dirname, join } from 'node:path'
 import argParser from 'yargs-parser';
 import { commandAliases } from './lib/commandAliases.js'
-
-let help = ''
+import help from './lib/help.js'
 
 function cli(): void {
   const command = argParser(process.argv.slice(2), {
@@ -13,17 +9,7 @@ function cli(): void {
     configuration: { 'camel-case-expansion': false }
   });
 
-
-
   if (command.help || (process.argv.length <= 2 && process.stdin.isTTY)) {
-    if (!help) {
-      const currentDir = dirname(fileURLToPath(import.meta.url))
-      console.log(currentDir)
-      const helpDir = join(currentDir, 'lib', 'help.md')
-      console.log(helpDir)
-      help = readFileSync(helpDir, 'utf8')
-      console.log(help)
-    }
     console.log(`\n${help}\n`);
   } else {
     console.log('Interactive configuration');
