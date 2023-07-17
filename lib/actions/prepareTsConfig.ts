41f6f5e03009aa2e8695d3a3b3233aab17c79def
Ant Stanley <antstanley@gmail.com>
2023-07-17 09:59:29 +0200

    feat: add prepare tsconfig.json

--- lib/actions/prepareTsConfig.ts
+++ lib/actions/prepareTsConfig.ts
@@ -1,43 +1,44 @@
 import { join } from 'node:path'
-import { writeFileSync } from 'node:fs'
+import { writeFileSync, existsSync, mkdirSync } from 'node:fs'
 
-const cdkTsConfig = `
-  {
-    "compilerOptions": {
-      "target": "ES2020",
-      "module": "commonjs",
-      "lib": [
-        "es2020",
-      ],
-      "declaration": true,
-      "strict": true,
-      "noImplicitAny": true,
-      "strictNullChecks": true,
-      "noImplicitThis": true,
-      "alwaysStrict": true,
-      "noUnusedLocals": false,
-      "noUnusedParameters": false,
-      "noImplicitReturns": true,
-      "noFallthroughCasesInSwitch": false,
-      "inlineSourceMap": true,
-      "inlineSources": true,
-      "experimentalDecorators": true,
-      "strictPropertyInitialization": false,
-      "typeRoots": [
-        "./node_modules/@types"
-      ]
-    },
-    "exclude": [
-      "node_modules",
-      "cdk.out"
+const cdkTsConfig = {
+  "compilerOptions": {
+    "target": "ES2020",
+    "module": "commonjs",
+    "lib": [
+      "es2020",
+    ],
+    "declaration": true,
+    "strict": true,
+    "noImplicitAny": true,
+    "strictNullChecks": true,
+    "noImplicitThis": true,
+    "alwaysStrict": true,
+    "noUnusedLocals": false,
+    "noUnusedParameters": false,
+    "noImplicitReturns": true,
+    "noFallthroughCasesInSwitch": false,
+    "inlineSourceMap": true,
+    "inlineSources": true,
+    "experimentalDecorators": true,
+    "strictPropertyInitialization": false,
+    "typeRoots": [
+      "./node_modules/@types"
     ]
-  }
-`
+  },
+  "exclude": [
+    "node_modules",
+    "cdk.out"
+  ]
+}
 
-function prepareTsConfig(currentPath: string): boolean {
+
+function prepareTsConfig(currentPath: string, cdkPath: string): boolean {
   let result = false
   try {
-    writeFileSync(join(currentPath, 'tsconfig.cdk.json'), cdkTsConfig)
+    const fullCdkPath = join(currentPath, cdkPath)
+    if (!existsSync(fullCdkPath)) mkdirSync(fullCdkPath, { recursive: true })
+    writeFileSync(join(fullCdkPath, 'tsconfig.json'), JSON.stringify(cdkTsConfig, null, 2))
     result = true
   } catch (error) {
     console.warn(error)
---
+++ tests/actions/prepareTsConfig.test.ts
@@ -0,0 +1,30 @@
+import { describe, test, beforeAll, afterAll, expect } from 'vitest'
+import { mkdirSync, rmSync, existsSync, writeFileSync, readFileSync } from 'node:fs'
+import { join } from 'node:path'
+import prepareTsConfig from '../../lib/actions/prepareTsConfig.js'
+
+const workingDirBase = join(process.cwd(), 'prepare-cdktsconfig')
+
+const cdkPath = './cdk'
+
+describe('add cdk specific tsconfig file in cdk folder', () => {
+
+  beforeAll(() => {
+    if (existsSync(workingDirBase)) rmSync(workingDirBase, { recursive: true })
+    mkdirSync(workingDirBase, { recursive: true })
+  })
+
+  afterAll(() => {
+    // if (existsSync(workingDirBase)) rmSync(workingDirBase, { recursive: true })
+  })
+
+  test('create tsconfig.json in correct location with correct config', () => {
+    const result = prepareTsConfig(workingDirBase, cdkPath)
+    expect(result).toBe(true)
+    const getTsConfigJson = JSON.parse(readFileSync(join(workingDirBase, cdkPath, 'tsconfig.json'), 'utf8'))
+    expect(getTsConfigJson).toHaveProperty('compilerOptions')
+    expect(getTsConfigJson?.compilerOptions).toHaveProperty('module')
+    expect(getTsConfigJson?.compilerOptions?.module).toBe('commonjs')
+  })
+
+})
