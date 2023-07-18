fe799ffaf128c536f036d631e61df291c46540d2
Ant Stanley <antstanley@gmail.com>
2023-07-18 14:09:43 +0200

    feat: add gitignore and npmignore files

---
+++ lib/actions/updateGitNpmIgnore.ts
@@ -0,0 +1,42 @@
+import { readFileSync, writeFileSync, existsSync } from 'node:fs';
+import { join } from 'node:path';
+
+const ignoreSnippet = `
+# CDK asset staging directory
+.cdk.staging
+cdk.out
+`
+
+function updateGitNpmIgnore(currentPath: string) {
+
+  let result = false;
+  try {
+
+    const gitignorePath = join(currentPath, '.gitignore');
+    if (existsSync(gitignorePath)) {
+      const gitignoreString = readFileSync(gitignorePath, 'utf8');
+      const newGitIgnore = gitignoreString + ignoreSnippet
+      writeFileSync(gitignorePath, newGitIgnore);
+    }
+    else {
+      writeFileSync(gitignorePath, ignoreSnippet);
+    }
+
+    const npmignorePath = join(currentPath, '.npmignore');
+    if (existsSync(npmignorePath)) {
+      const npmIgnoreString = readFileSync(npmignorePath, 'utf8');
+      const newNpmIgnore = npmIgnoreString + ignoreSnippet
+      writeFileSync(npmignorePath, newNpmIgnore);
+    }
+    else {
+      writeFileSync(npmignorePath, ignoreSnippet);
+    }
+    result = true
+
+  }
+  catch (error) {
+    console.warn(error);
+  }
+  return result;
+}
+export default updateGitNpmIgnore;
